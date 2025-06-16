cfee6554a0af592b8bb160a7c71605cfce6935ec
sharipaxman <102460739+sharipaxman@users.noreply.github.com>
2025-06-16 11:45:15 +0500

    1 коммит

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        'title': 'Home',
+        'content' : 'Главная страница магазина - HOME',
+        'list' : ['first', 'second'],
+        'is_authenticated' : False 
+
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')

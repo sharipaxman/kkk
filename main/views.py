ee8ac1cc7c9f7dfb424b4f5d31dc19a3b3947da0
sharipaxman <102460739+sharipaxman@users.noreply.github.com>
2025-06-17 00:22:44 +0500

    1 версия

--- main/views.py
+++ main/views.py
@@ -4,13 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Home',
-        'content' : 'Главная страница магазина - HOME',
-        'list' : ['first', 'second'],
-        'is_authenticated' : False 
+        'title': ' Home - Главная',
+        'content' : 'Магазин мебели HOME',
+        
 
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': ' Home - О нас',
+        'content' : 'О нас',
+        'text_on_page' : 'Добрый день'
+        
+
+    }
+    return render(request, 'main/about.html', context)
